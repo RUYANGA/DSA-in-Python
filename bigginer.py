1e66136f865288843e8d66d954f32cfc00354b51
RUYANGA <ruyangam15@gmail.com>
2026-01-22 20:19:57 +0200

    condition

--- bigginer.py
+++ bigginer.py
@@ -5,3 +5,10 @@ print(f"Hello {full_name}")
 print(f"You are {ages} years old")
 print(f"Class has {student} numbers")
 print('hello')
+
+for_sele=True
+
+if for_sele:
+    print('This items is  for sales')
+else:
+    print('This items it not for sales')
