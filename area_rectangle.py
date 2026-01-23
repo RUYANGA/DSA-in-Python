69e36479182955fec885790bac042270c2f3a7d1
RUYANGA <ruyangam15@gmail.com>
2026-01-23 08:20:01 +0200

    area of rectangle

--- area_rectangle.py
+++ area_rectangle.py
@@ -1,5 +1,5 @@
-Length=float(input("Enter length?: "))
-Width=float(input("Enter width?: "))
+Length=float(input("Enter the length?: "))
+Width=float(input("Enter the width?: "))
 area=Length*Width
 
 print(f"The area of rectangle is {area} cm^2")
